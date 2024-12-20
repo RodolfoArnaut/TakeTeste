9190368dbec8f54983eb59da6d13ae7e9e5d7302
RodolfoArnaut <rodolfoarnaut@gmail.com>
2024-12-20 20:36:52 -0300

    API

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const express = require('express');
 const app = express();
 const githubRoutes = require('./src/routes/githubRoutes'); // Certifique-se de que este caminho está correto
@@ -20,26 +19,3 @@ app.use('/api', githubRoutes);
 app.listen(PORT, () => {
     console.log(`Servidor rodando na porta ${PORT}`);
 });
-=======
-const express = require('express');
-const app = express();
-const githubRoutes = require('./src/routes/githubRoutes'); // Certifique-se de que este caminho está correto
-const PORT = process.env.PORT || 4000;
-
-// Middleware para JSON
-app.use(express.json());
-
-// Middleware para logar todas as solicitações da API
-app.use((req, res, next) => {
-    console.log(`Recebida solicitação: ${req.method} ${req.originalUrl}`);
-    next();
-});
-
-// Rotas de API
-app.use('/api', githubRoutes);
-
-// Servidor
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-});
->>>>>>> b0f64481eff56e7b975454a5b98682c1ea045b87
